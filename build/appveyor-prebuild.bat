63ec5610b393506506e5e41ede8b75570644be1b
monemihir <monemihir@hotmail.com>
2017-11-01 10:55:54 +1100

    Adding prebuild step to checkout castle code.

---
+++ build/appveyor-prebuild.bat
@@ -0,0 +1,17 @@
+REM ~ Clone Castle Core and update to tag 3.0.0
+cd..
+git clone --no-checkout --progress --verbose https://github.com/castleproject/Core.git CastleCore
+cd CastleCore
+git checkout tags/3.0.0
+
+REM ~ Clone Castle Windsor and update to tag 3.0.0
+cd ..
+git clone --no-checkout --progress --verbose https://github.com/castleproject/Windsor.git CastleWindsor
+cd CastleWindsor
+git checkout tags/3.0.0
+
+REM ~ go back to build level
+cd..
+dir
+cd CastleWindsorExtensions
+
