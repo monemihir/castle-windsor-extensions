fe146bab23d47d9280d6b4194d9bb845e3d50f77
monemihir <monemihir@hotmail.com>
2017-11-01 11:16:22 +1100

    Removing deleting of .pdb files since we are building Release|AnyCPU config.

--- build/appveyor-postbuild.bat
+++ build/appveyor-postbuild.bat
@@ -3,7 +3,7 @@ rmdir /S /Q dist
 
 REM ~ copy binaries
 xcopy /I /Y "src\Castle.Windsor.Extensions\bin\Castle.Windsor.Extensions*.*" "dist\lib\net40"
-del "dist\lib\net40\*.pdb"
+REM ~ del "dist\lib\net40\*.pdb"
 
 REM ~ create archive
 7z a artifacts\Castle.Windsor.Extensions.zip dist
